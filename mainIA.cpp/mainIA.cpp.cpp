0eaae59cca738a83cba1c3e216607db49fd28e9e
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2023-05-22 20:59:53 +0700

    int main

--- mainIA.cpp/mainIA.cpp.cpp
+++ mainIA.cpp/mainIA.cpp.cpp
@@ -2,7 +2,32 @@
 #include <vector>
 using namespace std;
 
+#include "anak.h"
+#include "ibu.h"
 
+int main() {
+	ibu* varIbu1 = new ibu("Dini");
+	ibu* varIbu2 = new ibu("Novi");
+	anak* varAnak1 = new anak("Tono");
+	anak* varAnak2 = new anak("Rini");
+	anak* varAnak3 = new anak("Dewi");
+
+	varIbu1->tambahAnak(varAnak1);
+	varIbu1->tambahAnak(varAnak2);
+	varIbu1->tambahAnak(varAnak3);
+	varIbu2->tambahAnak(varAnak1);
+
+	varIbu1->cetakAnak();
+	varIbu2->cetakAnak();
+
+	delete varIbu1;
+	delete varIbu2;
+	delete varAnak1;
+	delete varAnak2;
+	delete varAnak3;
+
+	return 0;
+}
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
