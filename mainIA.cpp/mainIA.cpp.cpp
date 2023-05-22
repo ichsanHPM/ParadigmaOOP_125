e1240582a1404fae5199e9b1b7015b445c0cf1c0
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2023-05-22 20:56:06 +0700

    membuat kelas ibu

--- mainIA.cpp/mainIA.cpp.cpp
+++ mainIA.cpp/mainIA.cpp.cpp
@@ -2,10 +2,7 @@
 #include <vector>
 using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
